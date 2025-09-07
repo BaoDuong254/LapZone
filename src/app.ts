a29dc17bb5ccaf946da76c64671b608229cd5b7c
GiaBao <duonggiabao254@gmail.com>
2025-09-07 12:46:27 +0700

    feat: implement web routing with home and about pages

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import webRoutes from "./routes/web";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
+webRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
---
+++ src/routes/web.ts
@@ -0,0 +1,14 @@
+import express, {Express} from "express";
+const router = express.Router();
+
+const webRoutes = (app: Express) => {
+  router.get("/", (req, res) => {
+    res.render("home");
+  });
+  router.get("/about", (req, res) => {
+    res.send("About Page");
+  });
+  app.use("/", router);
+};
+
+export default webRoutes;

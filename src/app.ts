5d95aa67e8eac68279515b0fa9b52f1b243f678b
GiaBao <duonggiabao254@gmail.com>
2025-09-07 17:41:03 +0700

    Add user controller and create user view
    - Implemented user controller with methods for rendering home and create user pages.
    - Added EJS template for the create user page with basic HTML structure.

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,15 @@ import webRoutes from "./routes/web";
 const app = express();
 const port = process.env.PORT || 3000;
 
+// set view engine
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+
+// load routes
 webRoutes(app);
+
+// config static files
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,13 +1,10 @@
 import express, {Express} from "express";
+import { getCreateUserPage, getHomePage } from "../controllers/user.controller";
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
-  router.get("/", (req, res) => {
-    res.render("home");
-  });
-  router.get("/about", (req, res) => {
-    res.send("About Page");
-  });
+  router.get("/", getHomePage);
+  router.get("/create-user", getCreateUserPage);
   app.use("/", router);
 };
 

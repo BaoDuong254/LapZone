247844071e29bb8b78620963ab4742cf30e6c65e
GiaBao <duonggiabao254@gmail.com>
2025-09-13 15:51:15 +0700

    feat: enhance product creation with validation and error handling

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -2,16 +2,27 @@ import { Request, Response } from "express";
 import { ProductSchema, TProductSchema } from "validation/product.schema";
 
 const getAdminCreateProductPage = async (req: Request, res: Response) => {
-  return res.render("admin/product/create.ejs");
+  const errors: string[] = [];
+  const oldData = {
+    name: "",
+    price: "",
+    detailDesc: "",
+    shortDesc: "",
+    quantity: "",
+    factory: "",
+    target: "",
+  };
+  return res.render("admin/product/create.ejs", { errors, oldData });
 };
 
 const postAdminCreateProduct = async (req: Request, res: Response) => {
-  const { name } = req.body as TProductSchema;
-  try {
-    const result = ProductSchema.parse(req.body);
-    console.log(result);
-  } catch (error) {
-    console.log(error);
+  const { name, price, detailDesc, factory, quantity, shortDesc, target } = req.body as TProductSchema;
+  const validate = ProductSchema.safeParse(req.body);
+  if (!validate.success) {
+    const errorZod = validate.error.issues;
+    const errors = errorZod?.map((item) => `${item.message} (${String(item.path[0])})`);
+    const oldData = { name, price, detailDesc, factory, quantity, shortDesc, target };
+    return res.render("admin/product/create.ejs", { errors, oldData });
   }
   return res.redirect("/admin/product");
 };

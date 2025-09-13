0f710badc692d2c44fffb3d8a82609eb39e21a0d
GiaBao <duonggiabao254@gmail.com>
2025-09-13 18:28:23 +0700

    feat: add item service with product retrieval functions
    - Implemented `getProducts` to fetch all products from the database.
    - Implemented `getProductById` to fetch a single product by its ID.

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
+import { getProductById } from "services/admin/product.service";
 
 const getProductPage = async (req: Request, res: Response) => {
-  return res.render("client/product/detail.ejs");
+  const { id } = req.params;
+  const product = await getProductById(Number(id));
+  return res.render("client/product/detail.ejs", { product: product });
 };
 
 export { getProductPage };
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { getProducts } from "services/client/item.service";
 import {
   getAllRoles,
   getAllUsers,
@@ -9,7 +10,8 @@ import {
 } from "services/user.service";
 
 const getHomePage = async (req: Request, res: Response) => {
-  return res.render("client/home/show.ejs");
+  const products = await getProducts();
+  return res.render("client/home/show.ejs", { products: products });
 };
 
 const getCreateUser = async (req: Request, res: Response) => {
